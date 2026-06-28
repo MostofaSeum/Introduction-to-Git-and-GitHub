312fc0000cbceb9c4157b9a5669fea923f4ab15b
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-28 17:27:01 +0600

    feat: add check_reboot script and associated documentation screenshot

--- Module-2/check_reboot.py
+++ Module-2/check_reboot.py
@@ -1,4 +1,5 @@
 def check_reboot():
-    """Check if the system needs a reboot."""
+    "Check if the system needs a reboot."
+    print("Checking if the system needs a reboot.")
 
     return False
