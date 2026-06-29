02fbc964113c19fa85c5307e35eb9b96fdd784a0
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-29 10:21:59 +0600

    Added some files

--- Module-2/check_reboot.py
+++ Module-2/check_reboot.py
@@ -2,5 +2,6 @@ def check_reboot():
     "Check if the system needs a reboot."
     print("Checking if the system needs a reboot.")
     print("Hello World")
+    print("Checking the changes")
 
     return False
